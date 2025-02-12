70040aa9c386dc393503b777672ed9e2c2c50910
x-zerox <ajprofessor0@gmail.com>
2025-02-12 13:58:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ files = glob.glob(ppath)
 JisshuBot.start()
 loop = asyncio.get_event_loop()
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHANNEL_ID = -1002253779892
 
 async def Jisshu_start():
     print('\n')
